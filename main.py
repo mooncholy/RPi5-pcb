f536a2b56bf0987f8d19d0b14e8dd12234da82bd
mooncholy <m.y.hassan3@student.aast.edu>
2024-06-22 17:38:26 +0300

    serial tx to arduino

--- main.py
+++ main.py
@@ -1,13 +1,43 @@
 from ultralytics import YOLO
 import argparse
+import math, serial, time
+
+temp_arr = []
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", required=True, default='./models/best.pt')
     args = parser.parse_args()
+    classNames = ['mouse_bite', 'spur', 'missing_hole', 'short', 'open_circuit', 'spurious_copper']
+    ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+    ser.reset_input_buffer()
     model = YOLO(args.model)
     results = model.predict('tcp://192.168.137.36:8080', stream=True, imgsz=416)
     while True:
         for result in results:
             boxes = result.boxes
-            probs = result.probs
+            # probs = result.probs
+            for box in boxes:
+                probs = math.ceil((box.conf[0]*100))/100
+                print(f"Confidence = {probs}")
+                # class name
+                cls = int(box.cls[0])
+                cls = str(cls) + "\n"
+                if len(temp_arr) < 1:
+                    temp_arr.append(cls)
+                    #byte_index = bytes(temp_arr[-1])
+                    #tx = byte_index + b'\n'
+                    #print(f"tx = {tx}")
+                    x = str.encode(temp_arr[-1])
+                    print(x)
+                    ser.write(x)
+                    temp_arr = []
+                elif cls != temp_arr[-1]:
+                    temp_arr.append(cls)
+                    #bytes_index = bytes(temp_arr[-1])
+                    #tx = byte_index + b'\n'
+                    #print(f"tx = {tx}")
+                    x = str.encode(temp_arr[-1])
+                    print(x)
+                    ser.write(x)
+                    temp_arr = []
